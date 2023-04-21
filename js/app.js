a59987dea294c58cf755e8d6829e6d58773bee24
jingwenlin123 <jingwenlin123@gmail.com>
2023-04-20 20:05:31 -0400

    updated function code for makeAccount button and logout button

--- js/app.js
+++ js/app.js
@@ -5,6 +5,72 @@ let endpoint01 = "https://jchvf589sh.execute-api.us-east-1.amazonaws.com/default
 
 /* SUPPORTING FUNCTIONS */
 
+// feature 1: create new account
+
+let signupController = () => {
+
+	$("#signup_message").html("");
+	$("#signup_message").removeClass();
+
+	// first, let's do some client side error trapping
+	let newfirstname = $("#newfirstname").val();
+    let newlastname = $("#newlastname").val();
+    let newusername =  $("#newusername").val()
+	let newpassword = $("#newpassword").val() //want its value, based on form input from html page
+	let newpassword2 = $("#newpassword2").val()
+
+	// if there's empty content...
+	if(newusername == "" || newfirstname =="" || newlastname == "" || newpassword== "" || newpassword2== ""){
+		$("#signup_message").html("All fields are required. Please try again.");
+		$('#signup_message').addClass("alert alert-danger text-center");
+	return; //to stop the argument :D
+	}
+
+	// if the password is too short
+	if(newpassword.length < 8 || newpassword2.length < 8){
+		$("#signup_message").html("Password is too short. Please try again.");
+		$('#signup_message').addClass("alert alert-danger text-center");
+	return; //to stop the argument :D
+	}
+
+	// if password do not match...
+	if(newpassword != newpassword2){
+		$("#signup_message").html("The passwords do not match. Please try again.");
+		$('#signup_message').addClass("alert alert-danger text-center");
+	return; //to stop the argument :D
+	}
+	
+
+	// serialize the data from the form 
+	let the_serialized_data = $("#form-signup").serialize();
+	console.log("the_serialized_data: "+the_serialized_data);
+	// now calling the ajax call
+	$.ajax({
+		url: endpoint01 + "/users",
+		data: the_serialized_data,
+		method: "POST",
+		success: (results) => {
+			console.log(results)
+            $("#fname").val(newfirstname);
+            $("#lname").val(newlastname);
+			$("#username").val(newusername); //so all the author needs to do is to click at the sign in
+			$("#password").val(newpassword);
+			$(".content-wrapper").hide();
+			$("#div-login").show();
+		},
+
+		error: (data) => { //any problem from the API
+			console.log(data)
+			//console.log(data["responseJSON"])
+			//console.log(data.responseJSON)
+			//$('#signup_message').html('Username may be already in use');
+			$('#signup_message').html(data["responseJSON"]);
+			$('#signup_message').addClass("alert alert-danger text-center");
+		}
+	});
+
+}
+
 // feature 0, get login info 
 
 let loginController = () => {
@@ -46,7 +112,7 @@ let loginController = () => {
 			$("#container-2col").show(); //show the 2 column container
 			$('#div-cards').show();   //show the default page
 			// refresh the cards page
-			cardsController();
+			//cardsController();
 		},
 		error: (data) => {
 			// login failed.  Remove userid 
@@ -76,7 +142,7 @@ $(document).ready( () => {
 		$("#createdby2").val(localStorage.userid);
 		$("#createdby3").val(localStorage.userid);
 		// refresh the data
-		cardsController();
+		//cardsController();
 	}
 
     // if no one is sign in.
@@ -97,6 +163,10 @@ $(document).ready( () => {
 		loginController();
 	});
 
+    $('#btnMakeAccount').click(() => {
+		signupController();
+	});
+
     $('#btnSignup').click( () => {
 		$(".content-wrapper").hide(); // it hides the divs :D 	
 		$('#div-signup').show(); 
@@ -107,6 +177,13 @@ $(document).ready( () => {
 		$('#div-login').show();   //show the default page
 	});
 
+	/* what happens if the logout link is clicked? */
+	$('#link-logout').click( () => {
+		// First ... remove userid from localstorage
+		localStorage.removeItem("userid");
+		// Now force the page to refresh
+		window.location = "./index.html";
+	});
 
 
 	
